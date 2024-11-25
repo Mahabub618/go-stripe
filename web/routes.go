4afa4da54686a8f3cbc471248592723b327cbc09
Mahabub Rahman <mahabubrahman618@gmail.com>
2024-11-26 01:13:06 +0600

    Implement driver for db connection

--- web/routes.go
+++ web/routes.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"runtime"
+
+	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,6 +16,19 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+
+	mux.Get("/charge-once", app.ChargeOnce)
+
+	_, currentFile, _, _ := runtime.Caller(0)
+	projectRoot := filepath.Join(filepath.Dir(currentFile), "../..")
+
+	err := os.Chdir(projectRoot)
+	if err != nil {
+		log.Fatal("Error changing working directory: ", err)
+	}
+
+	fileServer := http.FileServer(http.Dir("./cmd/internal/static"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	return mux
 }
