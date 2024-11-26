edd06d890a27d42f1aaae08fe2db5112e655e184
Mahabub Rahman <mahabubrahman618@gmail.com>
2024-11-27 02:24:57 +0600

    Implement model for DB connection and remove amount field from buy once page form

--- web/handlers.go
+++ web/handlers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"myapp/cmd/internal/models"
+	"net/http"
+)
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
@@ -49,8 +52,21 @@ func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["publishable_key"] = app.config.stripe.key
 	stringMap["API"] = app.config.api
+
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
 	if err := app.renderTemplate(w, r, "buy-once", &templateData{
 		StringMap: stringMap,
+		Data:      data,
 	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
